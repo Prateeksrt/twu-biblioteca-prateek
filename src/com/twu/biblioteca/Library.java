9c489fba839d92e3abd0ffff0e90c9ec351dfa04
Prateeksrt <prateeksrt2009@gmail.com>
2015-04-21 11:16:53 +0530

    Refactored Library.

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -11,6 +11,36 @@ public class Library {
 
     private List<String> availableServices;
 
+    public String getWelcomeMsg(){
+        return "Welcome! to Bibloteca.";
+    }
+
+    public String checkOut(int bookNumber){
+        try {
+            transferBookForAvailableToIssued(bookNumber - 1);
+            return "Thank you! Enjoy the book";
+        } catch (Exception e){
+            return "That book is not available.";
+        }
+    }
+
+    public String returnBook(final String bookName){
+        Book bookToReturn = getIssuedBookByName(bookName);
+        if(bookToReturn!=null){
+            transferBookForIssuedToAvailable(bookToReturn);
+            return "Thank you for returning the book.";
+        }
+        return "That is not a valid book to return.";
+    }
+
+    public List<Book> getAvailableBooks(){
+        return availableBooks;
+    }
+
+    public List<String> getAvailableServices() {
+        return availableServices;
+    }
+
     public Library(){
         availableBooks = new ArrayList<>();
         availableBooks.add(new Book("My Book", "xyz", "2000"));
@@ -23,25 +53,23 @@ public class Library {
 
     }
 
-    public String getWelcomeMsg(){
-        return "Welcome! to Bibloteca.";
+    private void transferBookForIssuedToAvailable(Book issuedBook){
+        issuedBooks.remove(issuedBook);
+        availableBooks.add(issuedBook);
     }
 
-    public List<Book> getAvailableBooks(){
-        return availableBooks;
+    private void transferBookForAvailableToIssued(int bookNumber) {
+        Book toBeIssued = availableBooks.remove(bookNumber);
+        issuedBooks.add(toBeIssued);
     }
 
-    public List<String> getAvailableServices() {
-        return availableServices;
-    }
-
-    public String checkOut(int bookNumber){
-        try {
-            issuedBooks.add(availableBooks.remove(bookNumber - 1));
-            return "Thank you! Enjoy the book";
-        } catch (Exception e){
-            return "That book is not available.";
+    private Book getIssuedBookByName(String bookName){
+        for(Book issuedBook : this.issuedBooks) {
+            if (issuedBook.getName().equals(bookName)) {
+                return issuedBook;
+            }
         }
+        return null;
     }
 
     @Override
@@ -49,25 +77,5 @@ public class Library {
         return "Library{" +
                 "availableBooks=" + availableBooks +
                 '}';
-    }
-
-    public String returnBook(final String bookName){
-        Book bookToReturn = null;
-        for(Book issuedBook : issuedBooks) {
-            if (issuedBook.getName().equals(bookName)) {
-                bookToReturn = issuedBook;
-            }
-        }
-
-        if(bookToReturn!=null){
-            transferBookForIssuedToAvailable(bookToReturn);
-            return "Thank you for returning the book.";
-        }
-        return "That is not a valid book to return.";
-    }
-
-    private void transferBookForIssuedToAvailable(Book issuedBook){
-        issuedBooks.remove(issuedBook);
-        availableBooks.add(issuedBook);
     }
 }
